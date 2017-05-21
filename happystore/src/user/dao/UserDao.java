d73a4823cd6d79461d31ccf116b2ee9348ab4082
polo <2282306404@qq.com>
2017-05-21 18:09:26 +0800

    4

--- happystore/src/user/dao/UserDao.java
+++ happystore/src/user/dao/UserDao.java
@@ -7,10 +7,16 @@ public interface UserDao {
 	// 用户注册
 	public void add(User user) throws Exception;
 
-	// 用户信息修改
-	public void update(User user) throws Exception;
+	// 用户基础信息修改
+	public void updateUser(User user) throws Exception;
 
-	// 通过用户名和密码获得用户，进行登陆
+	// 通过用户名和密码获得用户
 	public User getByUsernameAndPwd(String username, String password) throws Exception;
+
+	// 修改密码
+	public User updatePwd(String uid,String pwd) throws Exception;
+	
+	// 用户注销
+	public void cancel(String uid) throws Exception;
 
 }
--- happystore/src/user/dao/impl/UserDaoImpl.java
+++ happystore/src/user/dao/impl/UserDaoImpl.java
@@ -1,29 +1,59 @@
 package user.dao.impl;
 
+import java.sql.SQLException;
+
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 
-import com.itheima.utils.DataSourceUtils;
-
 import user.dao.UserDao;
 import user.entity.User;
+import user.utils.DataSourceUtils;
 
-public class UserDaoImpl implements UserDao{
+public class UserDaoImpl implements UserDao {
 
+	// 用户注册
 	public void add(User user) throws Exception {
-		
+		QueryRunner qr = new QueryRunner(DataSourceUtils.getDataSource());
+		String sql = "insert into user values(?,?,?,?,?,?,?,?,?);";
+		qr.update(sql, user.getUid(), user.getUsername(), user.getPassword(),
+				user.getName(), user.getEmail(), user.getTelephone(),
+				user.getBirthday(), user.getSex(), 0);
+
 	}
 
-	public void update(User user) throws Exception {
-		
+	// 用户基础信息修改
+	public void updateUser(User user) throws Exception {
+		QueryRunner qr = new QueryRunner(DataSourceUtils.getDataSource());
+		String sql = "update user set username = ?,name = ?,email = ?,birthday = ?,telephone = ?,sex = ? where uid =? ";
+		qr.update(sql, user.getUsername(), user.getName(), user.getEmail(),
+				user.getBirthday(), user.getTelephone(), user.getSex(),
+				user.getUid());
 	}
 
+	// 通过用户名和密码获得用户
 	public User getByUsernameAndPwd(String username, String password)
 			throws Exception {
 		QueryRunner qr = new QueryRunner(DataSourceUtils.getDataSource());
-		String sql="select * from user where username = ? and password = ? limit 1";
-		return qr.query(sql, new BeanHandler<>(User.class), username,password);
+		String sql = "select * from user where username = ? and password = ? limit 1";
+		return qr.query(sql, new BeanHandler<>(User.class), username, password);
 	}
 
- 
+	// 修改密码
+	@Override
+	public User updatePwd(String uid, String pwd) throws Exception {
+		QueryRunner qr = new QueryRunner(DataSourceUtils.getDataSource());
+		String sql = "update user set password = ? where uid =?";
+		qr.update(sql, new BeanHandler<>(User.class), pwd, uid);
+		sql = "select * from user where uid = ? limit 1";
+		return qr.query(sql, new BeanHandler<>(User.class), uid);
+	}
+
+	// 用户注销
+	@Override
+	public void cancel(String uid) throws Exception {
+		QueryRunner qr = new QueryRunner(DataSourceUtils.getDataSource());
+		String sql = "update user set state = ? where uid =?";
+		qr.update(sql, 1, uid);
+	}
+
 }
